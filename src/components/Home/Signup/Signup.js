adc20909719b4a6309af1f2fd56bf0ea5708dcd1
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-18 03:30:41 +0600

    signup with email and password implemented

--- src/components/Home/Signup/Signup.js
+++ src/components/Home/Signup/Signup.js
@@ -1,6 +1,8 @@
 import React, { useRef, useState } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useCreateUserWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocialLogin from '../../SocialLogin/SocialLogin';
 import './Signup.css';
 
@@ -8,10 +10,41 @@ const Signup = () => {
     const [agree, setAgree] = useState(false);
     const nameRef = useRef('');
     const emailRef = useRef('');
-    const passworsRef = useRef('');
+    const passwordRef = useRef('');
     const confirmPasswordRef = useRef('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading,
+        error,
+      ] = useCreateUserWithEmailAndPassword(auth);
+
     const handleSignUp = (event) => {
-        event.preventDefault()
+        event.preventDefault();
+
+        const name = nameRef.current.value;
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+        const confirmPassword = confirmPasswordRef.current.value;
+
+        createUserWithEmailAndPassword(email, password);
+
+    }
+    const from = location?.state?.from?.pathname || '/';
+    if(user)
+    {
+        navigate(from , {replace:true});
+    }
+    let errorElement;
+    if(error){
+        if(error){
+            console.log(error.message);
+            errorElement = <p className='text-danger'>An Error Occured. Please try again.</p>
+    
+        }
     }
     return (
         <div className='container signup-form-container'>
@@ -19,21 +52,22 @@ const Signup = () => {
                 <h2 className='form-title'>Sign Up</h2>
             </div>
             <Form onSubmit={handleSignUp} className='login-form'>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="text" placeholder="Enter name" required/>
+            <Form.Group className="mb-3" controlId="formBasicName">
+                    <Form.Control type="text" ref={nameRef} placeholder="Enter name" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    <Form.Control type="email" placeholder="Enter email" required/>
+                    <Form.Control type="email" ref={emailRef} placeholder="Enter email" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control type="password" ref={passwordRef} placeholder="Password" required/>
                 </Form.Group>
-                <Form.Group className="mb-3" controlId="formBasicPassword">
-                    <Form.Control type="password" placeholder="Confirm Password" />
+                <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+                    <Form.Control type="password" ref={confirmPasswordRef} placeholder="Confirm Password" required/>
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicCheckbox">
                     <Form.Check onClick={() => setAgree(!agree)} type="checkbox" label="I Agree to the Terms and Conditions" />
                 </Form.Group>
+                {errorElement}
                 <div className='text-center mb-3'>
                     <Button className='login-btn' type="submit" disabled={!agree}>
                         Sign Up
