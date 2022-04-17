5621d84a93dbc9f4960c31b06c851397255c7ee6
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-18 04:05:29 +0600

    login with email and password implemented

--- src/components/Home/Login/Login.js
+++ src/components/Home/Login/Login.js
@@ -1,28 +1,58 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { Link } from 'react-router-dom';
+import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+import auth from '../../../firebase.init';
 import SocialLogin from '../../SocialLogin/SocialLogin';
 import './Login.css';
 
 const Login = () => {
+    const emailRef = useRef('');
+    const passwordRef = useRef('');
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const [
+        signInWithEmailAndPassword,
+        user,
+        loading,
+        error,
+    ] = useSignInWithEmailAndPassword(auth);
+    const handleLogIn = (event) => {
+        event.preventDefault();
+
+        const email = emailRef.current.value;
+        const password = passwordRef.current.value;
+
+        signInWithEmailAndPassword(email, password)
+    }
+    const from = location?.state?.from?.pathname || '/';
+    if(user)
+    {
+        navigate(from , {replace:true});
+    }
+    let errorElement;
+    if(error){
+        if(error){
+            console.log(error.message);
+            errorElement = <p className='text-danger'>An Error Occured. Please try again.</p>
+    
+        }
+    }
     return (
         <div className='container login-form-container'>
             <div className='form-title-container'>
                 <h2 className='form-title'>Login</h2>
             </div>
-            <Form className='login-form'>
+            <Form onSubmit={handleLogIn} className='login-form'>
                 <Form.Group className="mb-3" controlId="formBasicEmail">
-                    {/* <Form.Label>Email address</Form.Label> */}
-                    <Form.Control type="email" placeholder="Enter email"  required/>
-                    {/* <Form.Text className="text-muted">
-                        We'll never share your email with anyone else.
-                    </Form.Text> */}
+                    <Form.Control ref={emailRef} type="email" placeholder="Enter email" required />
                 </Form.Group>
                 <Form.Group className="mb-3" controlId="formBasicPassword">
-                    {/* <Form.Label>Password</Form.Label> */}
-                    <Form.Control type="password" placeholder="Password" required/>
+                    <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
                 <p>Don't Have An Account? <Link to='/signup'>Sign up</Link> </p>
+                {errorElement}
                 <div className='text-center'>
                     <Button className='login-btn' type="submit">
                         Login
@@ -34,7 +64,7 @@ const Login = () => {
                 <p>Or</p>
                 <div></div>
             </div>
-            <SocialLogin/>
+            <SocialLogin />
         </div>
     );
 };
