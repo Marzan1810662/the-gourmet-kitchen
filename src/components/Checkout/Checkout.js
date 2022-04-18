e8bf6e8d366501168e5130c8c802c1928761214c
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-18 13:28:55 +0600

    swwet alert added on checkout page

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,15 +1,26 @@
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
+import swal from 'sweetalert';
 import auth from '../../firebase.init';
 import './Checkout.css';
 
 const Checkout = () => {
     const [user, loading, error] = useAuthState(auth);
+    
+    const handleCheckoutSubmit =(event) => {
+        event.preventDefault();
+
+        swal({
+            title: "The Gourmet Kitchen",
+            text: "Thank You for Choosing The Gourmwet Kitchen!",
+            icon: 'success',
+          });
+    }
     return (
         <div>
             <div className='checkout-form-container'>
             <h2>Checkout</h2>
-               <form>
+               <form onSubmit={handleCheckoutSubmit}>
                <input type="text" name="name" id="name" value={user.displayName}/>
                 <input type="text" name="email" id="email" value= {user.email} readOnly/>
                 <input type="text" name="phone" id="phone" placeholder='Phone Number' required/>
