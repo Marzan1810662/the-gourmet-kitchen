2254d21d7a381a4c0dcbd4a20f120ca05ba4a1f5
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-18 21:30:46 +0600

    login with facebook implemented

--- src/components/SocialLogin/SocialLogin.js
+++ src/components/SocialLogin/SocialLogin.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { faFacebook, faGoogle } from '@fortawesome/free-brands-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useSignInWithGoogle } from 'react-firebase-hooks/auth';
+import { useSignInWithFacebook, useSignInWithGoogle } from 'react-firebase-hooks/auth';
 import './Sociallogin.css';
 import auth from '../../firebase.init';
 import { useLocation, useNavigate } from 'react-router-dom';
@@ -9,23 +9,25 @@ import { useLocation, useNavigate } from 'react-router-dom';
 const SocialLogin = () => {
     const navigate = useNavigate();
     const location = useLocation();
-    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    const [signInWithGoogle, googleUser, googleLoading, googleSigninError] = useSignInWithGoogle(auth);
+    const [signInWithFacebook, facebookUser, facebookLoading, facebookSigninError] = useSignInWithFacebook(auth);
 
     const from = location?.state?.from.pathname || '/';
-    if (user) {
+
+    if (googleUser || facebookUser) {
         navigate(from, { replace: true });
     }
     let errorElement;
-    if(error){
-        console.log(error.message);
-        errorElement = <p className='text-danger'>{error.message}</p>
+    if(googleSigninError || facebookSigninError){
+        console.log(googleSigninError.message);
+        errorElement = <p className='text-danger'>{googleSigninError?.message}{facebookSigninError?.message}</p>
 
     }
     return (
         <div className='d-flex flex-column align-items-center social-container'>
             {errorElement}
             <button onClick={() => signInWithGoogle()} className='google-login-btn'><FontAwesomeIcon className='me-2' icon={faGoogle}></FontAwesomeIcon>Continue with Google</button>
-            <button className='facebook-login-btn'><FontAwesomeIcon className='me-2' icon={faFacebook}></FontAwesomeIcon>Continue with Facebook</button>
+            <button onClick={() => signInWithFacebook()} className='facebook-login-btn'><FontAwesomeIcon className='me-2' icon={faFacebook}></FontAwesomeIcon>Continue with Facebook</button>
         </div>
     );
 };
