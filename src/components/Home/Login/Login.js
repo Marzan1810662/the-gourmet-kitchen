2ff39458252e0252cba0ea35e34f6e36874fffbd
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-18 15:01:50 +0600

    send password reset email implemented

--- src/components/Home/Login/Login.js
+++ src/components/Home/Login/Login.js
@@ -1,7 +1,9 @@
 import React, { useRef } from 'react';
 import { Button, Form } from 'react-bootstrap';
-import { useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
+import { useSendPasswordResetEmail, useSignInWithEmailAndPassword } from 'react-firebase-hooks/auth';
 import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import auth from '../../../firebase.init';
 import LoadingSpinner from '../../LoadingSpinner/LoadingSpinner';
 import SocialLogin from '../../SocialLogin/SocialLogin';
@@ -19,6 +21,10 @@ const Login = () => {
         loading,
         error,
     ] = useSignInWithEmailAndPassword(auth);
+    const [sendPasswordResetEmail, sending, resetPasswordError] = useSendPasswordResetEmail(
+        auth
+    );
+
     const handleLogIn = (event) => {
         event.preventDefault();
 
@@ -27,6 +33,7 @@ const Login = () => {
 
         signInWithEmailAndPassword(email, password);
     }
+
     const from = location?.state?.from?.pathname || '/';
     if (user) {
         console.log(user);
@@ -40,9 +47,20 @@ const Login = () => {
 
         }
     }
-     if (loading) {
-         console.log('spinner from login');
+    if (loading) {
+        console.log('spinner from login');
         return <LoadingSpinner></LoadingSpinner>;
+    }
+
+    const handlePasswordreset = async () => {
+        const email = emailRef.current.value;
+        if (email) {
+            await sendPasswordResetEmail(email);
+            toast('Email sent');
+        }
+        else {
+            toast('Enter your email');
+        }
     }
     return (
         <div className='container login-form-container'>
@@ -56,13 +74,14 @@ const Login = () => {
                 <Form.Group className="mb-3" controlId="formBasicPassword">
                     <Form.Control ref={passwordRef} type="password" placeholder="Password" required />
                 </Form.Group>
-                <p>Don't Have An Account? <Link to='/signup'>Sign up</Link> </p>
+                <p>Forgot Password?<span onClick={handlePasswordreset} className='password-reset-span'>Reset</span></p>
                 {errorElement}
                 <div className='text-center'>
                     <Button className='login-btn' type="submit">
                         Login
                     </Button>
                 </div>
+                <p className='mt-2'>Don't Have An Account? <Link to='/signup'>Sign up</Link> </p>
             </Form>
             <div className='mt-2 d-flex align-items-center divider-div'>
                 <div></div>
@@ -70,6 +89,7 @@ const Login = () => {
                 <div></div>
             </div>
             <SocialLogin />
+            <ToastContainer/>
         </div>
     );
 };
