661f7d603e7b092ce1579f5ef2e7af764a84fabb
Marzan1810662 <hassanmarzan17@gmail.com>
2022-04-18 17:28:44 +0600

    responsive behavior added to about

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -4,12 +4,12 @@ import developer from '../../images/developer.jpg'
 
 const About = () => {
     return (
-        <div>
+        <div >
             <div className='my-2 py-2 my-md-4 py-md-4 '>
                 <h2 className='services-title'>About The Developer</h2>
                 <div className='title-underline-div'></div>
             </div>
-            <div className='d-flex justify-content-space-evenly align-items-center container about-container'>
+            <div className='d-md-flex justify-content-space-evenly align-items-center container about-container'>
                 <div className='description-div'>
                     <h3>Hi! I am <br /> <span  className='fw-bold'> Marzan Binte Hassan</span></h3>
                     <p>I am an aspiring developer. I am very passionate about web development and want to persue my career as a developer. Currently I am learning React js - a javascript library. As react is easier and popular among other js libraries, I am preparing myself to join the industry as a react developer. </p>
